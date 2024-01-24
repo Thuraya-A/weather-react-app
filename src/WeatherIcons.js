76ce375de2b615ef9d1b6cd0f4d84c010a8298e2
Thuraya-A <thuraya.a493@gmail.com>
2024-01-24 10:19:42 +0000

    updated the icons

--- src/WeatherIcons.js
+++ src/WeatherIcons.js
@@ -1,47 +1,97 @@
 import React from "react";
-import { WiDaySunny } from "weather-icons-react";
-import { WiNightClear } from "weather-icons-react";
-import { WiDayCloudy } from "weather-icons-react";
-import { WiCloud } from "weather-icons-react";
-import { WiCloudy } from "weather-icons-react";
-import { WiDayRainMix } from "weather-icons-react";
-import { WiNightAltRainMix } from "weather-icons-react";
-import { WiNightAltRain } from "weather-icons-react";
-import { WiNightAltThunderstorm } from "weather-icons-react";
-import { WiDaySnowWind } from "weather-icons-react";
-import { WiNightAltSnowWind } from "weather-icons-react";
-import { WiDayFog } from "weather-icons-react";
-import { WiNightFog } from "weather-icons-react";
+import {
+  WiDaySunny,
+  WiNightClear,
+  WiDayCloudy,
+  WiCloud,
+  WiCloudy,
+  WiDayRainMix,
+  WiNightAltRainMix,
+  WiNightAltRain,
+  WiNightAltThunderstorm,
+  WiDaySnowWind,
+  WiNightAltSnowWind,
+  WiDayFog,
+  WiNightFog,
+  WiDayThunderstorm,
+} from "weather-icons-react";
 
-export default function WeatherData(props) {
-  class WeatherIcons extends React.Component() {
-    render() {
-      const codeMapping = {
-        "01d": "WiDaySunny",
-        "01n": "WiNightClear",
-        "02d": "WiDayCloudy",
-        "02n": "WiDayCloudy",
-        "03d": "WiCloud",
-        "03n": "WiCloud",
-        "04d": "WiCloudy",
-        "04n": "WiCloudy",
-        "09d": "WiDayRainMix",
-        "09n": "WiNightAltRainMix",
-        "10d": "WiNightAltRainMix",
-        "10n": "WiNightAltRain",
-        "11d": "WiDayThunderstorm",
-        "11n": "WiNightAltThunderstorm",
-        "13d": "WiDaySnowWind",
-        "13n": "WiNightAltSnowWind",
-        "50d": "WiDayFog",
-        "50n": "WiNightFog",
-      };
+export default function WeatherIcon(props) {
+  const codeMapping = {
+    "01d": "WiDaySunny",
+    "01n": "WiNightClear",
+    "02d": "WiDayCloudy",
+    "02n": "WiDayCloudy",
+    "03d": "WiCloud",
+    "03n": "WiCloud",
+    "04d": "WiCloudy",
+    "04n": "WiCloudy",
+    "09d": "WiDayRainMix",
+    "09n": "WiNightAltRainMix",
+    "10d": "WiNightAltRainMix",
+    "10n": "WiNightAltRain",
+    "11d": "WiDayThunderstorm",
+    "11n": "WiNightAltThunderstorm",
+    "13d": "WiDaySnowWind",
+    "13n": "WiNightAltSnowWind",
+    "50d": "WiDayFog",
+    "50n": "WiNightFog",
+  };
 
-      return (
-        <div>
-          <{codeMapping[props.code]} size={24} color="#000" />
-        </div>
-      );
-    }
+  if (codeMapping[props.code] === "WiDaySunny") {
+    return <WiDaySunny size={24} />;
   }
+  if (codeMapping[props.code] === "WiNightClear") {
+    return <WiNightClear size={24} />;
+  }
+  if (codeMapping[props.code] === "WiDayCloudy") {
+    return <WiDayCloudy size={24} />;
+  }
+  if (codeMapping[props.code] === "WiDayCloudy") {
+    return <WiDayCloudy size={24} />;
+  }
+  if (codeMapping[props.code] === "WiCloud") {
+    return <WiCloud size={24} />;
+  }
+  if (codeMapping[props.code] === "WiCloud") {
+    return <WiCloud size={24} />;
+  }
+  if (codeMapping[props.code] === "WiCloudy") {
+    return <WiCloudy size={24} />;
+  }
+  if (codeMapping[props.code] === "WiCloudy") {
+    return <WiCloudy size={24} />;
+  }
+  if (codeMapping[props.code] === "WiDayRainMix") {
+    return <WiDayRainMix size={24} />;
+  }
+  if (codeMapping[props.code] === "WiNightAltRainMix") {
+    return <WiNightAltRainMix size={24} />;
+  }
+  if (codeMapping[props.code] === "WiNightAltRainMix") {
+    return <WiNightAltRainMix size={24} />;
+  }
+  if (codeMapping[props.code] === "WiNightAltRain") {
+    return <WiNightAltRain size={24} />;
+  }
+  if (codeMapping[props.code] === "WiDayThunderstorm") {
+    return <WiDayThunderstorm size={24} />;
+  }
+  if (codeMapping[props.code] === "WiNightAltThunderstorm") {
+    return <WiNightAltThunderstorm size={24} />;
+  }
+  if (codeMapping[props.code] === "WiDaySnowWind") {
+    return <WiDaySnowWind size={24} />;
+  }
+  if (codeMapping[props.code] === "WiNightAltSnowWind") {
+    return <WiNightAltSnowWind size={24} />;
+  }
+  if (codeMapping[props.code] === "WiDayFog") {
+    return <WiDayFog size={24} />;
+  }
+  if (codeMapping[props.code] === "WiNightFog") {
+    return <WiNightFog size={24} />;
+  }
+
+  return <div></div>;
 }
